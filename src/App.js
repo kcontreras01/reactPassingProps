3e33edc7c5ddb8955820ade20bc392628701fe22
kcontreras01 <kiara@hashtag-labs.com>
2018-10-31 14:52:16 -0400

    all divs change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Show from './show';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      show1: false,
+      show2: false,
+      show3: false,
+      show4: false,
+      visible: true
+    }
+  }
+
+    showHey = () => {
+        this.setState(prevState => ({ show1: !prevState.show1, visible: !prevState.visible}));
+    }
+
+    showBonjour = () => {
+        this.setState(prevState => ({ show2: !prevState.show2}));
+    }
+
+    showHai = () => {
+        this.setState(prevState => ({ show3: !prevState.show3}));
+    }
+
+    showOi = () => {
+        this.setState(prevState => ({ show4: !prevState.show4}));
+    }
+
+    setVisible = () => {
+      this.setState(prevState => ({ visible: !prevState.visible}))
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div onClick={this.showHey} >{this.state.show1 ? <Show greeting='ohhey'/> : 'hi'}</div>
+        <div onClick={this.showBonjour}>{this.state.show2 ? <Show greeting='bonjour'/> : 'hello'}</div>
+        <div onClick={this.showHai}>{this.state.show3 ? <Show greeting='ohai' /> : 'hola'}</div>
+        <div onClick={this.showOi}>{this.state.show4 ? <Show greeting='oi' /> : 'prego'}</div>
       </div>
     );
   }
---
+++ src/show.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Show extends Component {
+	render() {
+		return (
+				<div>{this.props.greeting}</div>
+			)
+	}
+}
+
+export default Show;
